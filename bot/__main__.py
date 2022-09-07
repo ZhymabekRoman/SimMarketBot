a188390b245ddb1a76777cbe18ab139ddf86de79
ZhymabekRoman <robanokssamit@yandex.ru>
2022-09-07 09:02:40 +0600

    [fix] Minor fix

--- bot/__main__.py
+++ bot/__main__.py
@@ -8,7 +8,6 @@ from aiogram import types
 from loguru import logger
 import asyncio
 import random
-import json
 
 
 async def poll_manager():
@@ -24,10 +23,12 @@ async def poll_manager():
     await asyncio.sleep(random.randint(10, 60))
     loop.create_task(sim_service.cache_updater())
 
+
 async def backup_sender_while(user_id: int, waiting_time: int = 3600):
     while True:
         await asyncio.sleep(waiting_time)
         await backup_sender(bot, user_id)
+
 
 async def on_bot_startup(dp: Dispatcher):
     # Регистрация /-команд в интерфейсе бота
@@ -45,19 +46,13 @@ async def on_bot_startup(dp: Dispatcher):
     await bot.send_message(chat_id=config.ADMIN_ID, text="Бот включен")
 
 
-    """
-    with open("robaa.txt", "w") as file:
-        json.dump(await sim_service._countries_list(), file, sort_keys=True, indent=4)
-    with open("hobaa.txt", "w") as file:
-        json.dump(await sim_service._numbers_status(0), file, sort_keys=True, indent=4)
-    """
-
 async def on_bot_shutdown(dp: Dispatcher):
     logger.info("Close sessions ...")
     await qiwi_wallet.close()
     await sim_service.shutdown()
     await bot.send_message(chat_id=config.ADMIN_ID, text="Бот выключен")
     config.export_to_file("bot/user_data/config.toml")
+
 
 # Start Telegram bot polling
 executor.start_polling(dp, on_startup=on_bot_startup, on_shutdown=on_bot_shutdown)  # , skip_update=True
